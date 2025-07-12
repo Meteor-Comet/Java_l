5ac67d9a4ad188f4921c130fe07d907e82a263d0
Toby Armstrong <513578967@qq.com>
2025-07-12 19:28:44 +0800

    更新代码

---
+++ src/main/java/questions/plane_ticket.java
@@ -0,0 +1,34 @@
+package questions;
+import java.util.Scanner;
+
+public class plane_ticket {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入机票价格：");
+        double price = sc.nextDouble();
+        System.out.println("请输入月份：");
+        int month = sc.nextInt();
+        System.out.println("请输入舱位(0为经济舱，1为头等舱):");
+        int class_ = sc.nextInt();
+        switch(month) {
+            case 5,6,7,8,9,10:
+                price = getPrice(price,class_,0.95,0.8);
+                break;
+            case 11,12,1,2,3,4:
+                price = getPrice(price,class_,0.85,0.7);
+                break;
+            default:
+                System.out.println("输入错误！");
+                return;
+        }
+        System.out.println("机票价格："+price);
+        }
+
+        static double getPrice(double price,int class_,double v0,double v1) {
+            if (class_==0) {
+                price *= v0;
+            }
+            else  {price *=v1;}
+            return price;
+        }
+}
---
+++ src/main/java/questions/prime_num.java
@@ -0,0 +1,19 @@
+package questions;
+
+public class prime_num {
+    public static void main(String[] args) {
+        int count = 0;
+        for(int i = 101; i < 201; i++){
+            for(int j = 2; j < Math.sqrt(i); j++){
+                if(i % j == 0){
+                    break;
+                }
+                else if(j == (int)Math.sqrt(i)){
+                    System.out.println(i);
+                    count++;
+                }
+            }
+        }
+        System.out.println("101-200的素数有"+count+"个");
+    }
+}
