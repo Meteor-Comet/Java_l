2963b8482685f9196edc5f8dade6912ed5a727bf
Toby Armstrong <513578967@qq.com>
2025-07-12 16:45:57 +0800

    更新代码

--- src/main/java/org/example/zhishu.java
+++ src/main/java/org/example/zhishu.java
@@ -7,11 +7,15 @@ public class zhishu {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入一个整数:");
         int a = sc.nextInt();
-        for(int i=2;i<a;i++){
+        //解释为什么循环条件是Math.sqrt(a)
+        //因为如果a是素数，那么a的平方根的整数部分小于a
+        //所以i<Math.sqrt(a)
+        for(int i=2;i<Math.sqrt(a);i++){
             if(a%i==0){
                 System.out.println("不是素数");
                 return;
             }
         }
+        System.out.println("是素数");
     }
 }
