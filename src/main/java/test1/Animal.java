fad41c7b6a8c162a6d44a8c3026f9edc36515a1a
Toby Armstrong <513578967@qq.com>
2025-07-14 18:16:04 +0800

    更新代码

---
+++ src/main/java/test1/Animal.java
@@ -0,0 +1,39 @@
+package test1;
+
+public class Animal {
+    private String brand;
+    private String name;
+    private int age;
+
+    public void setBrand(String brand)
+    {
+        this.brand = brand;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void show()
+    {
+        System.out.println(" name:" + name + "brand:" + brand + " age:" + age);
+    }
+}
