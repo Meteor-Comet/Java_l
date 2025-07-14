eeaa86840afe26c8d4cb189712a6b5e043dc0e55
Toby Armstrong <513578967@qq.com>
2025-07-14 13:05:45 +0800

    更新代码

--- src/main/java/questions/Red_Wars.java
+++ src/main/java/questions/Red_Wars.java
@@ -1,4 +1,18 @@
 package questions;
 
+import java.util.Random;
+
 public class Red_Wars {
+    public static void main(String[] args) {
+        int [] arr = {2,588,888,1000,10000};
+        Random r = new Random();
+        int index = arr.length-1;
+        for(int i=0;i<arr.length;i++){
+            int index_r = r.nextInt(index+1);
+            int red = arr[index_r];
+            arr[index_r] = arr[index];
+            index--;
+            System.out.println("抽到的红包是"+red);
+        }
+    }
 }
