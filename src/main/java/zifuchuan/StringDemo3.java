b879b74054c085832f19be8c4aa3eac422b8226d
Toby Armstrong <513578967@qq.com>
2025-07-15 23:34:34 +0800

    字符串

---
+++ src/main/java/zifuchuan/StringDemo3.java
@@ -0,0 +1,14 @@
+package zifuchuan;
+
+import java.util.Scanner;
+
+public class StringDemo3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个字符串：");
+        String str = sc.next();
+        str = str.toUpperCase();
+        String str2 = "ABC";
+        System.out.println(str==str2);
+    }
+}
