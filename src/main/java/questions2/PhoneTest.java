e38ebc94becf39c9463c5491a79c1e34da531089
Toby Armstrong <513578967@qq.com>
2025-07-15 16:48:25 +0800

    更新Car

---
+++ src/main/java/questions2/PhoneTest.java
@@ -0,0 +1,16 @@
+package questions2;
+
+public class PhoneTest {
+    public static void main(String[] args) {
+        Phone[] phones = new Phone[3];
+        phones[0] = new Phone("华为", 5999, "蓝色");
+        phones[1] = new Phone("苹果", 9999, "黑色");
+        phones[2] = new Phone("小米", 3999, "白色");
+        int sum = 0;
+        for (int i = 0; i < phones.length; i++) {
+            sum += phones[i].getPrice();
+        }
+        int avg = sum / phones.length;
+        System.out.println("平均价格是：" + avg);
+    }
+}
