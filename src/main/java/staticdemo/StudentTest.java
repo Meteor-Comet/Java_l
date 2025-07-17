272ef3310f25726e83c9b0c1ee5996f0aa645bc9
Toby Armstrong <513578967@qq.com>
2025-07-17 21:31:22 +0800

    静态方法

--- src/main/java/staticdemo/StudentTest.java
+++ src/main/java/staticdemo/StudentTest.java
@@ -1,20 +1,20 @@
 package staticdemo;
 
+import util.StudentUtil;
+
+import java.util.ArrayList;
+
 public class StudentTest {
     public static void main(String[] args) {
-        Student.teacherName = "王老师";
-        Student student1 = new Student();
-        student1.setName("张三");
-        student1.setAge(18);
-        student1.setGender("男");
-        student1.study();
-        System.out.println(student1);
-        Student student2 = new Student();
-        student2.setName("李四");
-        student2.setAge(19);
-        student2.setGender("女");
-        student2.study();
-        System.out.println(student2 );
+        ArrayList<Student> students = new ArrayList<>();
+        Student s1 = new Student("张三", 18, "男");
+        Student s2 = new Student("李四", 19, "女");
+        Student s3 = new Student("王五", 20, "男");
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
 
+        int maxage = StudentUtil.getMaxAge(students);
+        System.out.println("最大年龄是："+maxage);
     }
 }
--- src/main/java/util/StudentUtil.java
+++ src/main/java/util/StudentUtil.java
@@ -1,4 +1,19 @@
 package util;
 
+import staticdemo.Student;
+
+import java.util.ArrayList;
+
 public class StudentUtil {
+    private StudentUtil() {}
+
+    public static int getMaxAge(ArrayList<Student>  students) {
+        int maxAge = students.get(0).getAge();
+        for(Student s : students){
+            if(s.getAge() > maxAge){
+                maxAge = s.getAge();
+            }
+        }
+        return maxAge;
+    }
 }
