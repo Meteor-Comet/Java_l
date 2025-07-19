8a1326573476448a9edfb7abdd752a73ec3f0004
Toby Armstrong <513578967@qq.com>
2025-07-19 21:44:29 +0800

    static

---
+++ src/main/java/com/examlpe/codeblock/Test.java
@@ -0,0 +1,8 @@
+package com.examlpe.codeblock;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s = new Student();
+        Student t = new Student("张三",18);
+    }
+}
