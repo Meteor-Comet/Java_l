7b2699114c82fedb93d2464125b822973d3f0e51
Toby Armstrong <513578967@qq.com>
2025-07-22 13:20:27 +0800

    内部类

--- src/main/java/com/内部类/Outer.java
+++ src/main/java/com/内部类/Outer.java
@@ -1,8 +1,16 @@
 package com.内部类;
 
 public class Outer {
+    private int a =10;
 
     class Inner{
-
+        private int a =20;
+        public void show(){
+            int a = 30;
+            System.out.println(a);
+            System.out.println(this.a);
+            System.out.println(Outer.this.a);
         }
+    }
+
     }
--- src/main/java/com/内部类/Test.java
+++ src/main/java/com/内部类/Test.java
@@ -1,4 +1,10 @@
 package com.内部类;
 
 public class Test {
+    public static void main(String[] args) {
+        Outer.Inner oi = new Outer().new Inner();
+        oi.show();
+
+
+    }
 }
