e11681c19d776608408e11d94e49ae3900c4d45a
Toby Armstrong <513578967@qq.com>
2025-08-06 15:19:28 +0800

    Hash

--- src/main/java/数据结构/HashSetDemo1.java
+++ src/main/java/数据结构/HashSetDemo1.java
@@ -4,10 +4,20 @@ import java.util.HashSet;
 
 public class HashSetDemo1 {
     public static void main(String[] args) {
-        HashSet<String> set = new HashSet<>();
+        HashSet<Student> set = new HashSet<>();
         Student s1 = new Student("张三", 18);
         Student s2 = new Student("张三", 18);
-        System.out.println(s1.hashCode());
-        System.out.println(s2.hashCode());
+        Student s3 = new Student("王五", 20);
+        set.add(s1);
+        set.add(s2);
+        set.add(s3);
+        System.out.println(set);
+
+        HashSet<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(3);
+        System.out.println(set1);
     }
 }
---
+++ src/main/java/数据结构/LinkedHashSetDemo1.java
@@ -0,0 +1,19 @@
+package 数据结构;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo1 {
+    public static void main(String[] args) {
+        Student s1 = new Student("张三", 18);
+        Student s2 = new Student("李四", 19);
+        Student s3 = new Student("王五", 20);
+        LinkedHashSet<Student> lhs = new LinkedHashSet<>();
+        lhs.add(s1);
+        lhs.add(s2);
+        lhs.add(s3);
+        for(Student s:lhs){
+            System.out.println(s);
+        }
+    }
+
+}
